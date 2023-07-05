f27ed8780fcb707d1c84f684f575eabaf407f71f
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-05 11:46:23 -0500

    deploy 3

--- auth_usuarios/settings.py
+++ auth_usuarios/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pzjy-4obi2$a^-w+h%0%*mepz#r=yh)c_em1-cc-tmlk&x&#_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
